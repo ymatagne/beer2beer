7cb26ae2f305ff236c2e5eea644d252fd7d3a90b
Thomas VERHOKEN <tverhoken@norsys.fr>
2014-12-02 20:39:00 +0100

    Correction AuthService + ajout "ng-cloak"

--- front/public/app/src/services/authService.js
+++ front/public/app/src/services/authService.js
@@ -1,24 +1,22 @@
 'use strict';
 
-services.service('AuthService', function Auth($http) {
-    return {
-        currentUser: function() {
-             $http.get('/api/auth/loggedin').then(function(user){
-                if (user !== '0'){
-                   return user;
-                }
-            });
-        },
-        register: function(user){
-            return $http.post('/api/auth/create', {user: user});
-        },
-        login: function(email, password) {
-            return $http.post('/api/auth/local', {email: email, password: password}).then(function(res){
-                return res.data;
-            });
-        },
-        logout: function(){
-            return $http.get('/api/auth/logout');
-        }
+services.service('AuthService', ['$http', function Auth($http) {
+    this.currentUser = function() {
+         return $http.get('/api/auth/loggedin').then(function(res){
+            if (res.data !== '0'){
+               return res.data;
+            }
+        });
     };
-});
+    this.register = function(user){
+        return $http.post('/api/auth/create', {user: user});
+    };
+    this.login = function(email, password) {
+        return $http.post('/api/auth/local', {email: email, password: password}).then(function(res){
+            return res.data;
+        });
+    };
+    this.logout = function(){
+        return $http.get('/api/auth/logout');
+    };
+}]);
