70289ff9b91edca62ec2ce1860ce76370a616489
Matagne <yan.matagne@gmail.com>
2014-12-21 14:43:58 +0100

    Fix erreur IHM

--- front/public/app/src/controllers/footerController.js
+++ front/public/app/src/controllers/footerController.js
@@ -1,5 +1,5 @@
 angular.module('b2b.controllers').controller('footerController', function ($translate, $scope) {
-    $scope.radioModel = $translate.use();
+    $scope.radioModel = $translate.use() ||$translate.preferredLanguage();
     $scope.changeLanguage = function (langKey) {
         $translate.use(langKey);
     };
